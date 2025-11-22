92583fc46b2e4557c0ce6f681462a5d305f71fb0
Bhumika <guravbhumika808@gmail.com>
2025-11-22 22:39:14 +0530

    Add frontend folder to main repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# Zerodha
+A full-stack Zerodha clone built with modern web technologies, replicating core trading features and real-time UI. Ideal for learning stock trading platform architecture and frontend-backend integration.
---
+++ dashboard/src/components/GeneralContext.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+import BuyActionWindow from "./BuyActionWindow";
+import SellActionWindow from "./SellActionWindow";
+
+const GeneralContext = React.createContext({
+  openBuyWindow: (uid) => {},
+  closeBuyWindow: () => {},
+  openSellWindow: (uid) => {},
+  closeSellWindow: () => {},
+});
+
+export const GeneralContextProvider = (props) => {
+  const [isBuyWindowOpen, setIsBuyWindowOpen] = useState(false);
+  const [selectedStockUID, setSelectedStockUID] = useState("");
+
+  const [isSellWindowOpen, setIsSellWindowOpen] = useState(false);
+  const [selectedSellStockUID, setSelectedSellStockUID] = useState("");
+
+  // --- Buy Handlers ---
+  const handleOpenBuyWindow = (uid) => {
+    setIsBuyWindowOpen(true);
+    setSelectedStockUID(uid);
+  };
+
+  const handleCloseBuyWindow = () => {
+    setIsBuyWindowOpen(false);
+    setSelectedStockUID("");
+  };
+
+  // --- Sell Handlers ---
+  const handleOpenSellWindow = (uid) => {
+    setIsSellWindowOpen(true);
+    setSelectedSellStockUID(uid);
+  };
+
+  const handleCloseSellWindow = () => {
+    setIsSellWindowOpen(false);
+    setSelectedSellStockUID("");
+  };
+
+  return (
+    <GeneralContext.Provider
+      value={{
+        openBuyWindow: handleOpenBuyWindow,
+        closeBuyWindow: handleCloseBuyWindow,
+        openSellWindow: handleOpenSellWindow,
+        closeSellWindow: handleCloseSellWindow,
+      }}
+    >
+      {props.children}
+
+      {/* Conditional render of Buy and Sell Windows */}
+      {isBuyWindowOpen && <BuyActionWindow uid={selectedStockUID} />}
+      {isSellWindowOpen && <SellActionWindow uid={selectedSellStockUID} />}
+    </GeneralContext.Provider>
+  );
+};
+
+export default GeneralContext;
